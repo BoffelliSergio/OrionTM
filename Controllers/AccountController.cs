4931264dfd61428b2e559a03469cfa3c779dfe07
SERGIO BOFFELLI <sergio_bofelli@terceiros.sicredi.com.br>
2023-02-21 20:14:48 -0300

    ajuste menu

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -81,6 +81,61 @@ namespace OrionTM_Web.Controllers
             return View(users);
         }
 
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+
+            return View();
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel registroVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = registroVM.UserName };
+                user.Email = registroVM.Email;
+                user.PhoneNumber = registroVM.PhoneNumber;
+
+                if (registroVM.Password != registroVM.PasswordConf)
+                {
+
+                    ModelState.AddModelError("Aviso", "Senha não confere!!!!");
+                    return View(registroVM);
+
+                }
+
+                var result = await _userManager.CreateAsync(user, registroVM.Password);
+
+                if (result.Succeeded)
+                {
+                    if (registroVM.IsAdm == true)
+                    {
+                        await _userManager.AddToRoleAsync(user, "Admin");
+                    }
+                    else
+                    {
+                        await _userManager.AddToRoleAsync(user, "Usuario");
+                    }
+
+                    return RedirectToAction("Status", "Home");
+                }
+                else
+                {
+                    this.ModelState.AddModelError("Registro", "Falha ao registrar o usuário");
+                }
+            }
+
+            return View(registroVM);
+
+        }
+
+     
+
         public IActionResult setlanguage(string culture)
 
         {
